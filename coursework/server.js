ea280b3b3589693286590184cb4790ef4d97e84e
eGathergood <ewan_good@hotmail.co.uk>
2018-04-24 21:29:42 +0100

    Changed partials

--- coursework/server.js
+++ coursework/server.js
@@ -1,19 +1,21 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/profiles";
+
+//Express setup
 const express = require('express');
 const app = express();
 const session = require('express-session');
 const bodyParser = require('body-parser');
 
+//Path var set
 var path = require('path');
 
 app.use(session({ secret: 'example' }));
 
-
-
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
