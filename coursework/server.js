4f445ec731ab3ce3cb987b8ab1e54711b14c2259
eGathergood <ewan_good@hotmail.co.uk>
2018-04-24 22:43:10 +0100

    public change

--- coursework/server.js
+++ coursework/server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + "/public"));
 
 var db;
 
