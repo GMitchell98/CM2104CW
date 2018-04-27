c0fe9476c61727d765a587e62d4db4b3fe6f1c87
eGathergood <ewan_good@hotmail.co.uk>
2018-04-27 14:51:58 +0100

    Revert last 2 commits

--- coursework/server.js
+++ coursework/server.js
@@ -59,7 +59,7 @@ app.get('/users', function(req, res) {
 });
 
 
-
+  
 app.get('/profile', function(req, res) {
   if(!req.session.loggedin){res.redirect('/login');return;}
   //get the requested user based on their username, eg /profile?username=dioreticllama
@@ -87,6 +87,7 @@ app.post('/logout', function(req, res) {
 });
 
 
+
 app.post('/dologin', function(req, res) {
   console.log(JSON.stringify(req.body))
   var uname = req.body.username;
@@ -110,7 +111,8 @@ app.post('/login', function(req, res) {
 var datatostore = {
 "username":req.body.username,
  "email":req.body.email,
-  "password":req.body.password}
+ "password":req.body.password,
+ "favdrink":req.body.favdrink}
 
 
 
